51d4c3b81767aff3fa2f051712487e9f3f320b70
joshparkerj <superfakek@gmail.com>
2018-09-23 17:24:20 -0600

    shorten the server code a lil

--- be/server.js
+++ be/server.js
@@ -1,6 +1,3 @@
-// server.js
-// 9/17/2018
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -10,36 +7,25 @@ const events = require('./events');
 app.use(bodyParser.json());
 app.use(cors({origin: ['http://localhost:3000']}));
 
-app.get('/health', (req,res) => {
-  res.send('ok');
-  console.log('health check ok');
-})
-
 app.get('/all-events/', (req,res) => {
   res.send(events.readEvent());
 })
 
 app.get('/events', (req,res) => {
-  const id = req.query.id;
-  res.send(events.eventById(id));
+  res.send(events.eventById(req.query.id));
 })
 
 app.delete('/events/:id', (req,res) => {
-  if (events.deleteEvent(Number(req.params.id))){
-    res.status(200).send();
-  }else{
-    res.status(404).send();
-  }
+  res.status(events.deleteEvent(Number(req.params.id)) ? 200 : 404).send();
 })
 
 app.post('/events', (req,res) => {
   const date = new Date(req.body.date);
-  const Event = {
+  res.send(events.createEvent({
     name: req.body.name,
     time: date.toLocaleTimeString(),
     date: date.toDateString()
-  }
-  res.send(events.createEvent(Event));
+  }));
 })
 
 app.put('/events/:id', (req,res) => {
