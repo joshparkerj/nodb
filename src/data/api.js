92b1ef27770d975be0777301b73132970daaaf48
joshparkerj <superfakek@gmail.com>
2018-09-23 18:04:06 -0600

    edit down the api code a bit

--- src/data/api.js
+++ src/data/api.js
@@ -1,65 +1,55 @@
 import axios from 'axios';
 
-const API_ADDRESS = 'http://localhost:8080/events';
+const API = 'http://localhost:8080/';
+
+const err = err => {
+  console.error(err);
+  return err;
+}
 
 export function createEvent(Event){
-  return axios.post(API_ADDRESS, {
+  return axios.post(`${API}events`, {
     name: Event.name,
     date: new Date(`${Event.date} ${Event.time}`)
   })
     .then(res => {
       return res.data.id;
     })
-    .catch(err => {
-      console.error(err);
-      return err;
-    })
+    .catch(err)
 }
 
 export function readEvent(){
-  return axios.get('http://localhost:8080/all-events')
+  return axios.get(`${API}all-events`)
     .then(res => {
       return res.data;
     })
-    .catch(err => {
-      console.error(err);
-      return err;
-    })
+    .catch(err)
 }
 
 export function eventById(id){
-  return axios.get(`${API_ADDRESS}/?id=${id}`)
+  return axios.get(`${API}events/?id=${id}`)
     .then(res => {
       return res.data;
     })
-    .catch(err => {
-      console.error(err);
-      return err;
-    })
+    .catch(err)
 }
 
-export function updateEvent(Event, id){
-  return axios.put(`${API_ADDRESS}/${id}`, {
-    name: Event.name,
-    date: Event.date,
-    time: `${Event.time}:00`
+export function updateEvent(E, id){
+  return axios.put(`${API}events/${id}`, {
+    name: E.name,
+    date: E.date,
+    time: `${E.time}:00`
   })
     .then(res => {
       return res.data;
     })
-    .catch(err => {
-      console.error(err);
-      return err;
-    })
+    .catch(err)
 }
 
 export function deleteEvent(id){
-  return axios.delete(`${API_ADDRESS}/${id}`)
+  return axios.delete(`${API}events/${id}`)
     .then(res => {
       return res.status;
     })
-    .catch(err => {
-      console.error(err);
-      return err;
-    })
+    .catch(err)
 }
