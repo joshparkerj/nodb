0046143d9a26b186b76d7a47a42eb3f9588edf75
Josh Parker <jparker@sherpas.ai>
2022-02-22 18:41:39 -0700

    lint

--- events.js
+++ events.js
@@ -1,64 +1,62 @@
-const data = require('./data.json');
 const fs = require('fs');
+const data = require('./data.json');
 
-function writeFile(){
-  return new Promise( (res,rej) => {
-    fs.writeFile('./data.json',JSON.stringify(data),err => {
+function writeFile() {
+  return new Promise((res, rej) => {
+    fs.writeFile('./data.json', JSON.stringify(data), (err) => {
       if (err) rej(err);
       else res('ok');
-    })
-  })
+    });
+  });
 }
 
-function createEvent(Event){
-  Event.id = 1 + Math.max(...data.map(e => e.id));
-  data.push(Event);
+function createEvent(Event) {
+  const id = 1 + Math.max(...data.map((e) => e.id));
+  data.push({ ...Event, id });
   return writeFile()
-  .then(r => {
-    return {id: Event.id};
-  })
+    .then(() => ({ id }));
 }
 
-function readEvent(){
+function readEvent() {
   return data;
 }
 
-function eventById(id){
-  return data.filter(e => Number(e.id) === Number(id));
+function eventById(id) {
+  return data.filter((e) => Number(e.id) === Number(id));
 }
 
-function updateEvent(Event,id){
-  const E = data.find(e => Number(e.id) === Number(id));
-  if (E){
-    for (let i in Event){
+function updateEvent(Event, id) {
+  const E = data.find((e) => Number(e.id) === Number(id));
+  if (E) {
+    for (const i in Event) {
       E[i] = Event[i];
     }
+
     return writeFile()
-    .then(r => {
-      return E;
-    })
+      .then(() => E);
   }
+
   return null;
 }
 
-function deleteEvent(id){
-  const i = data.findIndex(e => Number(e.id) === Number(id));
-  if (i !== -1){
-    data.splice(i,1);
+function deleteEvent(id) {
+  const i = data.findIndex((e) => Number(e.id) === Number(id));
+  if (i !== -1) {
+    data.splice(i, 1);
+
     return writeFile()
-    .then(r => {
-      return true;
-    })
+      .then(() => true);
   }
+
   return false;
 }
 
 const events = {
-  createEvent: createEvent,
-  readEvent: readEvent,
-  updateEvent: updateEvent,
-  deleteEvent: deleteEvent,
-  eventById: eventById
+  createEvent,
+  readEvent,
+  updateEvent,
+  deleteEvent,
+  eventById,
 };
 
 module.exports = events;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const cors = require('cors');
-const events = require('./events');
 const rateLimit = require('express-rate-limit');
 
+const events = require('./events');
+
+const app = express();
+
 app.use(bodyParser.json());
-app.use(cors({origin: ['http://localhost:3000']}));
+app.use(cors({ origin: ['http://localhost:3000'] }));
 
 app.use(express.static('./build'));
 
@@ -17,51 +19,50 @@ app.use(rateLimit({
   standardHeaders: true,
 }));
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.sendFile('./build/index.html');
-})
+});
 
-app.get('/all-events/', (req,res) => {
+app.get('/all-events/', (req, res) => {
   res.send(events.readEvent());
-})
+});
 
-app.get('/events', (req,res) => {
+app.get('/events', (req, res) => {
   res.send(events.eventById(req.query.id));
-})
+});
 
-app.delete('/events/:id', (req,res) => {
+app.delete('/events/:id', (req, res) => {
   events.deleteEvent(Number(req.params.id))
-  .then(r => {
-    res.status(r ? 200 : 404).send();
-  })
-})
+    .then((r) => {
+      res.status(r ? 200 : 404).send();
+    });
+});
 
-app.post('/events', (req,res) => {
+app.post('/events', (req, res) => {
   const date = new Date(req.body.date);
   events.createEvent({
     name: req.body.name,
     time: date.toLocaleTimeString(),
-    date: date.toDateString()
+    date: date.toDateString(),
   })
-  .then(r => {
-    res.send(r);
-  })
+    .then((r) => {
+      res.send(r);
+    });
 });
 
+app.put('/events/:id', (req, res) => {
+  events.updateEvent(req.body, Number(req.params.id))
+    .then((r) => {
+      if (r) {
+        res.status(200).send(r);
+      } else {
+        res.status(404).send();
+      }
+    });
+});
 
-app.put('/events/:id', (req,res) => {
-  events.updateEvent(req.body,Number(req.params.id))
-  .then(r => {
-    if (r){
-      res.status(200).send(r);
-    }else{
-      res.status(404).send();
-    }
-  })
-})
-
-app.get('*', (req,res) => {
-  res.sendFile(__dirname + '/build/index.html');
-})
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/build/index.html`);
+});
 
 app.listen(process.env.PORT || 8080);
